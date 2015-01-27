5df373cd2f0fc0c5a3f8204a3048faade5b21ec8
Telegram Co <contact@telegram-co.com>
2015-01-27 10:52:37 -0500

    Blog post for 2015-01-27

---
+++ _site/makepost.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+DATE_STAMP="$(date +"%Y-%m-%d")"
+POST_PATH="_posts/$DATE_STAMP.md"
+ASSETS_PATH="assets/article_images/$DATE_STAMP"
+
+mkdir $ASSETS_PATH
+touch $POST_PATH
+
+echo "---
+layout: post
+title:  \"FILL ME IN\"
+date:   $DATE_STAMP 14:00 <-- FILL-ME-IN
+image:  /assets/article_images/$DATE_STAMP/FILL-ME-IN.jpg
+---
+" > $POST_PATH
