d68a39c11399481bec1b0918bdaf0e49de28801f
Telegram Co <contact@telegram-co.com>
2015-02-01 13:05:36 -0500

    Open the post for editing

--- makepost.sh
+++ makepost.sh
@@ -5,12 +5,19 @@ TIME_STAMP="$(date +"%H:%M")"
 POST_PATH="_posts/$DATE_STAMP.md"
 ASSETS_PATH="assets/article_images/$DATE_STAMP"
 
+# Make the assets dir
 mkdir -p $ASSETS_PATH
+
+# Make the post itself
 touch $POST_PATH
 
+# Stub out the post's header matter
 echo "---
 layout: post
 title:  \"FILL ME IN\"
 date:   $DATE_STAMP $TIME_STAMP
 ---
 " > $POST_PATH
+
+# Open the post for editing
+./edit.sh
