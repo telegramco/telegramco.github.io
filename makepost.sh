e4497f416d0460791f76066db9a160c18d2370ad
Telegram Co <contact@telegram-co.com>
2015-02-01 13:03:30 -0500

    Make makepost.sh a bit better

--- makepost.sh
+++ makepost.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
 
 DATE_STAMP="$(date +"%Y-%m-%d")"
+TIME_STAMP="$(date +"%H:%M")"
 POST_PATH="_posts/$DATE_STAMP.md"
 ASSETS_PATH="assets/article_images/$DATE_STAMP"
 
-mkdir $ASSETS_PATH
+mkdir -p $ASSETS_PATH
 touch $POST_PATH
 
 echo "---
 layout: post
 title:  \"FILL ME IN\"
-date:   $DATE_STAMP 14:00 <-- FILL-ME-IN
+date:   $DATE_STAMP $TIME_STAMP
 ---
 " > $POST_PATH
